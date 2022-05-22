67cd9656abd77cfc6b0bddbd5bd68f0d7b489f63
Naveen5767 <mknaveen5767@gmail.com>
2022-05-22 16:11:50 +0530

    Few lines of code added

---
+++ src/org/test/Example.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Example {
+	
+	int a=20;
+	int b=30;
+	
+	public  void empId() {
+		System.out.println("EmpId : 2345");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
